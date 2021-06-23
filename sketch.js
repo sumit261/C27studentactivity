404dee50ebe8dc76d34450459809cc89d2933817
sumit261 <sumitpanchal546@gmail.com>
2021-06-23 20:46:19 +0530

    slingshot

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg ;
+var log6;
+var slingshot;
 function preload(){    backgroundImg = loadImage("sprites/bg.png");
 }
 function setup(){
@@ -33,9 +35,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 =new Log(230,180,80,PI/2);
     bird = new Bird(100,100);
-
+    slingshot =new Slingshot(bird.body,log6.body);
 }
 
 function draw(){
@@ -64,6 +66,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
+    slingshot.display();
 }
